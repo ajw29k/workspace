edc644dd0af78ffc8c7a04276627ef19b53d1835
ajw29k <alswjdgma12@gmail.com>
2024-05-31 12:42:22 +0900

    1

---
+++ workspase_java/JavaBasic/src/변수와자료형/변수_4.java
@@ -0,0 +1,18 @@
+package 변수와자료형;
+
+public class 변수_4 {
+    public static void main(String[] args) {
+        //1. 변수는 선언 후 반드시 최초 값을 한 번은 할당
+        int num;
+        num = 5; //이 코드가 없어지면 오류 발생!!
+        System.out.println(num);
+
+        //변수의 선언 및 값 항당은 동시에 가능
+        int a;
+        a = 7;
+        int b = 10;
+        System.out.println(b);
+
+        //
+    }
+}
