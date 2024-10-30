029d59b1a182c2c080596993366119b5ce409fdf
ajw29k <alswjdgma12@gmail.com>
2024-10-30 17:46:56 +0900

    1

--- workspace_java/ArrayStudy/src/Main.java
+++ workspace_java/ArrayStudy/src/Main.java
@@ -4,6 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i = sc.nextInt();
-        
+        if(i >= 2000) {
+            if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0){
+                System.out.println(1);
+            }
+        }
+        else if(i){
+            if (i % 100 == 0 || i % 400 != 0){
+                System.out.println(1);
+            }
+        }
     }
 }
